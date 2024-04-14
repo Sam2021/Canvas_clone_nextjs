a9fd03076ab1ec738f958b7e6d7fee5c85911321
Smit Parikh <parikhsmit786@gmail.com>
2024-04-14 21:25:08 +0530

    update

--- app/(root)/transformation/add/[type]/page.tsx
+++ app/(root)/transformation/add/[type]/page.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import Header from '@/components/shared/Header'
+import { transformationTypes } from '@/constants'
 
-const AddPage = () => {
+
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
   return (
-    <div>
-      
-    </div>
+    <>
+     <Header 
+        title={transformation.title}
+        subtitle={transformation.subTitle}
+      />
+    </>
   )
 }
 
-export default AddPage
+export default AddTransformationTypePage
